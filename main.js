ae6cb5d14e63882015b45ca026254b6669e646cb
SaiArun-ai <72151107+SaiArun-ai@users.noreply.github.com>
2021-12-14 20:22:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,3 +23,73 @@ function Back(){
     document.getElementById("Main").style = "display:none;";
     window.alert("We have moved to the hand-decider segment");
 }
+var song = "";
+var leftX;
+var leftY;
+var rightX;
+var rightY;
+var leftC;
+var rightC;
+function preset(){
+    song = loadSound("music.mp3");
+}
+function setup(){
+    canvas = createCanvas(500,500);
+    canvas.position(525,150);
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video,pose);
+    poseNet.on("pose",gotPoses);
+}
+function pose(){
+    console.log("I Work");
+}
+function Play(){
+    console.log("Works");
+    song.play();
+    song.rate(1);
+    song.setVolume(1);
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        LeftC = results[0].pose.keypoints[9].score;
+        leftX = results[0].pose.leftWrist.x;
+        leftY = results[0].pose.leftWrist.y;
+        rightX = results[0].pose.rightWrist.x;
+        rightY = results[0].pose.rightWrist.y;
+        rightC = results[0].pose.keypoints[10].score;
+    }
+}
+function draw(){
+    image(video,0,0,500,500);
+    if(hand == left){
+        if(leftX<250){
+            fill("#C9AE5D");
+        stroke("#C9AE5D");
+        circle(leftX,leftY,40);
+        document.getElementById("Previous").innerHTML = "Song Name : Harry Potter";
+        song.play("music.mp3");
+        }else{
+            fill("#C9AE5D");
+        stroke("#C9AE5D");
+        circle(leftX,leftY,40);
+        document.getElementById("Previous").innerHTML = "Song Name : Harry Potter";
+        song.play("music.mp3");
+        }
+    }else if(hand == right){
+        if(rightX<250){
+            fill("#C9AE5D");
+        stroke("#C9AE5D");
+        circle(rightX,rightY,40);
+        document.getElementById("Previous").innerHTML = "Song Name : Harry Potter";
+        song.play("music.mp3");
+        }else{
+            fill("#C9AE5D");
+        stroke("#C9AE5D");
+        circle(rightX,rightY,40);
+        document.getElementById("Previous").innerHTML = "Song Name : Harry Potter";
+        song.play("music.mp3");
+        }
+    }
+}
